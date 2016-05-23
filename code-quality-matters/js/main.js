274d4c7b72480c3ed7136fc37561d7683b6f0c5c
Tina M. Kister <tmkister@gmail.com>
2016-05-23 17:28:51 -0400

    GitHub Permission Test

---
+++ code-quality-matters/js/main.js
@@ -0,0 +1,11 @@
+var name = 'Richard';
+
+if (name.length > 6) {
+console.log('hooRAY!');
+}
+
+function eatIceCream() {
+console.log('time for some mint chocolate chip and cookies and cream!');
+}
+
+eatIceCream();
